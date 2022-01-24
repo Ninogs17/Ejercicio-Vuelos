4b2fcf76611da1d1844555d1263fb496e8800e2b
Nino <ninogs170401@gmail.com>
2022-01-24 17:38:55 +0100

    V3

---
+++ arraysdb.php
@@ -0,0 +1,74 @@
+<?php
+
+$arrayDestino=array(
+    array("Vuelo" => "Vueling-AE12", "Destino" => "Milan"),
+    array("Vuelo" => "Volotea-EA21", "Destino" => "Roma"),
+    array("Vuelo" => "Iberia-WE44", "Destino" => "Estambul"),
+    array("Vuelo" => "Ryanair-EW44", "Destino" => "Madrid"),
+    array("Vuelo" => "Vueling-ZK87", "Destino" => "Lima"),
+    array("Vuelo" => "Volotea-UA21", "Destino" => "Bilbao"),
+    array("Vuelo" => "Iberia-WH44", "Destino" => "Roma"),
+    array("Vuelo" => "Ryanair-EL00", "Destino" => "Madrid"),
+    array("Vuelo" => "Vueling-AE12", "Destino" => "Roma"),
+    array("Vuelo" => "Volotea-EA21", "Destino" => "Roma")
+);
+
+
+
+$arrayFabrAvion=array(
+    array("Vuelo" => "Vueling-AE12", "Fabricante del avión" => "Boeing"),
+    array("Vuelo" => "Volotea-EA21", "Fabricante del avión" => "Airbus"),
+    array("Vuelo" => "Iberia-WE44", "Fabricante del avión" => "Airbus"),
+    array("Vuelo" => "Ryanair-EW44", "Fabricante del avión" => "Boeing"),
+    array("Vuelo" => "Vueling-ZK87", "Fabricante del avión" => "Airbus"),
+    array("Vuelo" => "Volotea-UA21", "Fabricante del avión" => "Boeing"),
+    array("Vuelo" => "Iberia-WH44", "Fabricante del avión" => "Boeing"),
+    array("Vuelo" => "Ryanair-EL00", "Fabricante del avión" => "Airbus"),
+);
+
+
+
+$arrayDViaje=array(
+    array("Vuelo" => "Vueling-AE12", "Duración viaje" => "120 minutos"),
+    array("Vuelo" => "Volotea-EA21", "Duración viaje" => "100 minutos"),
+    array("Vuelo" => "Iberia-WE44", "Duración viaje" => "340 minutos"),
+    array("Vuelo" => "Ryanair-EW44", "Duración viaje" => "45 minutos"),
+    array("Vuelo" => "Vueling-ZK87", "Duración viaje" => "600 minutos"),
+    array("Vuelo" => "Volotea-UA21", "Duración viaje" => "65 minutos"),
+    array("Vuelo" => "Iberia-WH44", "Duración viaje" => "90 minutos"),
+    array("Vuelo" => "Ryanair-EL00", "Duración viaje" => "50 minutos"),
+    array("Vuelo" => "Vueling-AE12", "Duración viaje" => "115 minutos"),
+    array("Vuelo" => "Volotea-EA21", "Duración viaje" => "100 minutos")
+    array("Vuelo" => "Iberia-WE44", "Duración viaje" => "85 minutos"),
+    array("Vuelo" => "Ryanair-EW44", "Duración viaje" => "360 minutos"),
+);
+
+
+
+$arrayNpasajeros=array(
+    array("Vuelo" => "Vueling-AE12", "Número de pasajeros" => "90 personas"),
+    array("Vuelo" => "Volotea-EA21", "Número de pasajeros" => "75 personas"),
+    array("Vuelo" => "Iberia-WE44", "Número de pasajeros" => "100 personas"),
+    array("Vuelo" => "Ryanair-EW44", "Número de pasajeros" => "60 personas"),
+    array("Vuelo" => "Vueling-ZK87", "Número de pasajeros" => "77 personas"),
+    array("Vuelo" => "Volotea-UA21", "Número de pasajeros" => "93 personas"),
+    array("Vuelo" => "Iberia-WH44", "Número de pasajeros" => "86 personas"),
+    array("Vuelo" => "Ryanair-EL00", "Número de pasajeros" => "75 personas"),
+    array("Vuelo" => "Vueling-AE12", "Número de pasajeros" => "87 personas"),
+    array("Vuelo" => "Volotea-EA21", "Número de pasajeros" => "97 personas"),
+    array("Vuelo" => "Iberia-WE44", "Número de pasajeros" => "81 personas"),
+    array("Vuelo" => "Ryanair-EW44", "Número de pasajeros" => "75 personas"),
+    array("Vuelo" => "Vueling-ZK87", "Número de pasajeros" => "87 personas"),
+    array("Vuelo" => "Volotea-UA21", "Número de pasajeros" => "90 personas"),
+    array("Vuelo" => "Iberia-WH44", "Número de pasajeros" => "84 personas"),
+    array("Vuelo" => "Ryanair-EL00", "Número de pasajeros" => "67 personas"),
+    array("Vuelo" => "Vueling-AE12", "Número de pasajeros" => "86 personas"),
+    array("Vuelo" => "Volotea-EA21", "Número de pasajeros" => "66 personas"),
+    array("Vuelo" => "Iberia-WE44", "Número de pasajeros" => "40 personas"),
+    array("Vuelo" => "Ryanair-EW44", "Número de pasajeros" => "77 personas"),
+    array("Vuelo" => "Vueling-ZK87", "Número de pasajeros" => "82 personas"),
+    
+);
+
+
+?>
